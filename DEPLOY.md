b2c79b00596effbf3db59097431ca4793859343f
Florin Gologan <gflo13@yahoo.com>
2023-03-06 14:40:19 +0200

    ElasticBeanstalk set up

---
+++ DEPLOY.md
@@ -0,0 +1,17 @@
+# Prerequisits
+
+- install eb
+
+# Deploy Process
+
+## Inittial set up
+
+```
+eb use solaris-demo-api
+```
+
+## Deploy
+
+```
+yarn deploy
+```
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { serve } from "./app";
 import { error } from "./logger";
 
-const PORT = 2091;
+const PORT = 8080;
 
 process.on("uncaughtException", (err) => {
   error("Uncaught exception occurred. Mock server will stop", err);
